516146e8b7acb03c473a7196cb2734fa1956dc22
AhmadAyman99 <ahmedayman.aaa555@gmail.com>
2021-12-28 18:28:54 +0200

    Add Routing

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CourseProject1
+# RecipeBook
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.0.4.
 
--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,16 +8,11 @@ import { RecipeService } from '../../recipe.service';
 })
 export class RecipeItemComponent implements OnInit {
 
-  constructor(private recipeService: RecipeService) { }
-
+  @Input() recipeItem: Recipe;
+  @Input() index: number;
+  
   ngOnInit(): void {
     //console.log("RecipeItem: ",this.recipeItem)
-  }
-
-  @Input() recipeItem : Recipe;
-
-  selectItem(){
-    this.recipeService.recipeSelected.emit(this.recipeItem)
   }
 
 }
